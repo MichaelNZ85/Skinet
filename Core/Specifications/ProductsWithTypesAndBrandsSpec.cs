80d51201e4db4b2d719994f377051073209a13db
MichaelNZ85 <michaelnz10@outlook.com>
2020-10-24 16:45:56 +1300

    Updated Products controller

--- Core/Specifications/ProductsWithTypesAndBrandsSpec.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSpec.cs
@@ -1,7 +1,10 @@
-namespace Core.Specifications
-{
-    public class ProductsWithTypesAndBrandsSpec
-    {
-        
+using Core.Entities;
+
+namespace Core.Specifications {
+    public class ProductsWithTypesAndBrandsSpec : BaseSpecification<Product> {
+        public ProductsWithTypesAndBrandsSpec() {
+            AddInclude(x => x.ProductType);
+            AddInclude(x => x.ProductBrand);
+        }
     }
 }
